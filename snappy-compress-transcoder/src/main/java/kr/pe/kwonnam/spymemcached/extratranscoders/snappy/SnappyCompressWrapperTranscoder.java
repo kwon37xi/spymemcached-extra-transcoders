d97300e7702e4c00346f3ff5f3b429caa69e1768
KwonNam Son <kwon37xi@gmail.com>
2016-01-05 01:46:35 +0900

    add constructor to SnappyCompressWrapperTranscoder

--- snappy-compress-transcoder/src/main/java/kr/pe/kwonnam/spymemcached/extratranscoders/snappy/SnappyCompressWrapperTranscoder.java
+++ snappy-compress-transcoder/src/main/java/kr/pe/kwonnam/spymemcached/extratranscoders/snappy/SnappyCompressWrapperTranscoder.java
@@ -3,6 +3,7 @@ package kr.pe.kwonnam.spymemcached.extratranscoders.snappy;
 import kr.pe.kwonnam.spymemcached.extratranscoders.compresswrapper.AbstractCompressionWrapperTranscoder;
 import kr.pe.kwonnam.spymemcached.extratranscoders.compresswrapper.Compressor;
 import kr.pe.kwonnam.spymemcached.extratranscoders.compresswrapper.Decompressor;
+import net.spy.memcached.transcoders.Transcoder;
 import org.xerial.snappy.Snappy;
 
 import java.io.IOException;
@@ -11,6 +12,12 @@ import java.io.IOException;
  * <a href="https://github.com/xerial/snappy-java">snappy-java</a> based compression wrapper spymemcached transcoder.
  */
 public class SnappyCompressWrapperTranscoder<T> extends AbstractCompressionWrapperTranscoder<T> implements Compressor, Decompressor {
+
+    public SnappyCompressWrapperTranscoder(Transcoder<T> wrappedTranscoder) {
+        setWrappedTranscoder(wrappedTranscoder);
+        setCompressor(this);
+        setDecompressor(this);
+    }
 
     @Override
     public byte[] compress(byte[] bytes) {
